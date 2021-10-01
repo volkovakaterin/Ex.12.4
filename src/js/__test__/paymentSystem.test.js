e9aa9945826bfb4ff5d649189f0f62320cecc1a0
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-10-02 01:52:54 +0400

    initial commit

--- src/js/__test__/paymentSystem.test.js
+++ src/js/__test__/paymentSystem.test.js
@@ -1,21 +1,9 @@
 /* eslint-disable linebreak-style */
 import PaymentSystemCheck from '../paymentSystemCheck';
 
-// describe('paymentSystem', () => {
-// const button = new Button();
-// button.buttonValid();
-// });
-
 test('paymentSystem', () => {
   const paymentSystem = new PaymentSystemCheck();
-  const cards = document.querySelectorAll('.card');
-  const array = [true, true, true, true, true, false, true];
   paymentSystem.check(30569309025904);
-  function checkClass() {
-    const arrayClass = [];
-    cards.forEach((element) => { if (element.classList.contains('blackout')) { checkClass.push(true); } else { checkClass.push(false); } });
-    return arrayClass;
-  }
-
-  expect(checkClass()).toEqual(array);
+  const checkClass = document.querySelector('diners_club').classList.contains('blackout');
+  expect(checkClass).toEqual(false);
 });
