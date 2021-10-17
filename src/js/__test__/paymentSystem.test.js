8fe4d9e9d0e659069a5a499d64eb668304a414dc
Ekaterina Volkova <volkovakaterin@gmail.com>
2021-10-18 01:06:48 +0400

    initial commit

--- src/js/__test__/paymentSystem.test.js
+++ src/js/__test__/paymentSystem.test.js
@@ -1,9 +1,12 @@
 /* eslint-disable linebreak-style */
+
 import PaymentSystemCheck from '../paymentSystemCheck';
 
 test('paymentSystem', () => {
   const paymentSystem = new PaymentSystemCheck();
   paymentSystem.check(30569309025904);
-  const checkClass = document.querySelector('diners_club').classList.contains('blackout');
-  expect(checkClass).toEqual(false);
+  const checkClass = !document.querySelector('diners_club').classList.contains('blackout');
+  expect(checkClass).toBeTruthy();
 });
+
+
--- webpack.common.js
+++ webpack.common.js
@@ -47,4 +47,7 @@ module.exports = {
       chunkFilename: '[id].css',
     }),
   ],
+  devServer: {
+    port: 9000,
+  },
 };
